7f2acce8ffa977060376bfa99b15ca79e2f624c1
Carsten <carsten@kopis.de>
2017-04-17 10:10:31 +0200

    Automatically replace mod ID and version on build

--- src/main/java/de/kopis/survivalrecipes/SurvivalRecipes.java
+++ src/main/java/de/kopis/survivalrecipes/SurvivalRecipes.java
@@ -15,8 +15,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @Mod(modid = SurvivalRecipes.MODID, version = SurvivalRecipes.VERSION)
 public class SurvivalRecipes
 {
-    public static final String MODID = "survivalrecipes";
-    public static final String VERSION = "1.2";
+    public static final String MODID = "@@MOD_ID@@";
+    public static final String VERSION = "@@MOD_VERSION@@";
 
     public class ItemElytraBase extends Item {
         public ItemElytraBase()
