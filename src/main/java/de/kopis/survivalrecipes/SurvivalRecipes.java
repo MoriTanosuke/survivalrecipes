872f36ef3b783c139515e62e9683ef46ec1a937d
Carsten <carsten@kopis.de>
2017-04-17 09:59:15 +0200

    Push to v1.2

--- src/main/java/de/kopis/survivalrecipes/SurvivalRecipes.java
+++ src/main/java/de/kopis/survivalrecipes/SurvivalRecipes.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class SurvivalRecipes
 {
     public static final String MODID = "survivalrecipes";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     public class ItemElytraBase extends Item {
         public ItemElytraBase()
